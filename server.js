b6285a563e763744b32a20571548e4c11af34037
Ubendren <30634644+Ubendren96@users.noreply.github.com>
2017-08-06 16:20:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/artical-one', function (req, res){
     res.send("This is artical-one");
 });
@@ -101,7 +105,6 @@ app.get('/:articleName',function(req, res){
     var articleName =req.params.articleName;
     res.send(createTemplate(articals[articleName]));
 });
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
