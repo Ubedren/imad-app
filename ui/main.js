60c1639648ab215f291ad5410d15a8f8b84c2b7b
Ubendren <30634644+Ubendren96@users.noreply.github.com>
2017-08-06 18:21:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var pic = document.getElementById('pic');
 var marginLeft = 0;
 function moveLeft(){
-    marginLeft = marginLeft + 5;
+    marginLeft = marginLeft - 5;
     pic.style.marginLeft = marginLeft + 'px';
 }
 pic.onclick = function(){
