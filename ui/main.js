f128e9aee924bd82e87f2354d3e71391ccb3517b
Ubendren <30634644+Ubendren96@users.noreply.github.com>
2017-08-08 22:09:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,22 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function () {
-    counter = counter +1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    // create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it into the varible
+    request.onreadystatechange = function(){
+        if(reques.onreadystatechange == XMLHttpReques.DONE) {
+            //Take some action
+            if(request.status ==200){
+                var counter =request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    //Make the request
+    request.open('GET','http://ubendren96.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
