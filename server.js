a403c3d4faf24c9c017189604158c9aa1ea854bf
Ubendren <30634644+Ubendren96@users.noreply.github.com>
2017-08-10 21:35:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ var names=[];
 app.get('/submit-name/:name', function (req, res) {
   var name=req.params.name;
   names.push(name);
-  res.send(JSON.stringfy(names));
+  res.send(JSON.stringify(names));
 });
 
 app.get('/ui/madi.png', function (req, res) {
