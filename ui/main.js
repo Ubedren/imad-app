bdbe942d4962c09add6298d2577e5d5166535880
Ubendren <30634644+Ubendren96@users.noreply.github.com>
2017-08-10 21:55:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onclick = function () {
     request.onreadystatechange = function(){
         if(request.readyState == XMLHttpRequest.DONE) {
             //Take some action
-            if(request.status ==200){
+            if(request.status ==200 ||request.status==304){
                 var names=request.responseText;
                 names=JSON.parse(names);
                 var counter =request.responseText;
