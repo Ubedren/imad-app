08ab8c452798dc7275bb99701d7f3e23b2e2763c
Ubendren <30634644+Ubendren96@users.noreply.github.com>
2017-08-11 23:06:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,28 +20,15 @@ button.onclick = function () {
     request.open('GET','http://ubendren96.imad.hasura-app.io/counter',true);
     request.send(null);
 };
-var submit = document.getElementById("submit_btn");
+
+var nameInput = document.getElementById('name');
+var name= nameInput.value;
+var submit = document.getElementById('submit_btn');
 submit.onclick=function(){
-    
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function(){
-        if(request.readyState == XMLHttpRequest.DONE){
-            //take action
-            if(request.status == 200){
-                var names=request.responseText;
-                names=JSON.parse(names);
-                var list='';
-                for(var i = 0;i<names.length;i++){
-                    list = '<li>' + names[i]+ '</li>';
-                }
-                var ul=document.getElementById("namelist");
-                ul.innerHTML=list;
-            }
-        }
-    };
-    var nameInput = document.getElementById("name");
-    var name = nameInput.value;
-    request.open('GET','http://ubendren96.imad.hasura-app.io/counter '+name,true);
-    request.send(null);            
+    var names = ['name1','name2'];
+    for(var i =0;i< names.length;i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML=list;
 };
