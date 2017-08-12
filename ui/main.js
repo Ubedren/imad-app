f8ed8c8e7ca6639c949f063b1f21afc145367a1d
Ubendren <30634644+Ubendren96@users.noreply.github.com>
2017-08-12 20:59:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,11 +48,28 @@ var nameInput=document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick=function(){
-    var names = ['name1','name2'];
-    var list='';
-    for(var i =0;i< names.length;i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML=list;
+    
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it into the varible
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status ==200 ){
+                var names = request.responseText;
+                var list='';
+                for(var i =0;i< names.length;i++){
+                list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
+        }
+    };
+    
+    //Make the request
+    request.open('GET','http://ubendren96.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+    
 };
