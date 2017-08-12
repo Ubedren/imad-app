b5820d9ffa9e4a77612bdb18e6b12c46fef0adfe
Ubendren <30634644+Ubendren96@users.noreply.github.com>
2017-08-12 20:42:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,3 +45,14 @@ submit.onclick=function(){
 
 //submit name
 var nameInput=document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick=function(){
+    var names = ['name1','name2'];
+    var list='';
+    for(var i =0;i< names.length;i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML=list;
+};
