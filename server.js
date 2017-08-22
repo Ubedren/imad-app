14bd67ca597fa4760c59360478e4fade964258fa
Ubendren <30634644+Ubendren96@users.noreply.github.com>
2017-08-22 23:19:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.post('/login',function(req,res){
    
    pool.query('SELECT * FROM "user" WHERE username=$1',[username],function(err,result){
       if(err){
-          res.status(403).send(err.toString());
+          res.status(500).send(err.toString());
       } 
       else{
           if(result.rows.length === 0){
