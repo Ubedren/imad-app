dba5ad07be15b5005bb710bfc7bf2a5db319a4bb
Ubendren <30634644+Ubendren96@users.noreply.github.com>
2017-08-22 23:11:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,11 +135,11 @@ app.post('/create-user',function(req,res){
    });
 });
 
-app.post('/loginr',function(req,res){
+app.post('/login',function(req,res){
    var username = req.body.username;
    var password = req.body.password;
    
-   pool.query('SELECT * FROM "user" (username) VALUES ($1)',[username],function(err,result){
+   pool.query('SELECT * FROM "user" WHERE username=$1',[username],function(err,result){
       if(err){
           res.status(403).send(err.toString());
       } 
