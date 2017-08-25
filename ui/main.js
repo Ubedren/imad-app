bf9beb4616d52d51b67420d364303a2667e510ed
Ubendren <30634644+Ubendren96@users.noreply.github.com>
2017-08-25 09:35:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,49 +1,4 @@
-var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function () {
-    // create a request object
-    var request = new XMLHttpRequest();
-    
-    //capture the response and store it into the varible
-    request.onreadystatechange = function(){
-        if(request.readyState == XMLHttpRequest.DONE) {
-            //Take some action
-            if(request.status ==200 ){
-                var counter =request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    
-    //Make the request
-    request.open('GET','http://ubendren96.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
-/*
-var nameInput = document.getElementById('name');
-var name= nameInput.value;
-var list='';
-var submit = document.getElementById('submit_btn');
-submit.onclick=function(){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(request.readyState == XMLHttpRequest.DONE) {
-            if(request.status ==200 ){
-                var names = request.responseText;
-                for(var i =0;i< names.length;i++){
-                    list += '<li>' + names[i] + '</li>';
-                }
-            var ul = document.getElementById('namelist');
-            ul.innerHTML=list;
-            }
-        }
-    };
-    
-};
-*/
-
-//submit name
+//submit username/password
 var submit = document.getElementById('submit_btn');
 submit.onclick=function(){
     
@@ -54,24 +9,25 @@ submit.onclick=function(){
         if(request.readyState == XMLHttpRequest.DONE) {
             //Take some action
             if(request.status ==200 ){
-                var names = request.responseText;
-                names=JSON.parse(names);
-                var list='';
-                for(var i =0;i< names.length;i++){
-                list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML=list;
+                console.log('user logged successfully');
+                alert('logged in successfully');
+            }else if(request.status == 403){
+                alert('Username/password is incorrect');
+            }else{
+                alert('Something went wrong on the server');
             }
         }
     };
 
-    var nameInput=document.getElementById('name');
-    var name = nameInput.value;
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
 
     //Make the request
-    request.open('GET','http://ubendren96.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
+    request.open('POST','http://ubendren96.imad.hasura-app.io/login');
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username,password: password}));
     
     
 };
