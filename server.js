7d20a3c10437f0b2891d868735fa67aba413e6c4
Ubendren <30634644+Ubendren96@users.noreply.github.com>
2017-08-25 10:36:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,6 +136,33 @@ app.post('/create-user',function(req,res){
    });
 });
 
+/*
+app.post('/login',function(req,res){
+
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('select * from "user" where username = $1',[username],function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      } 
+      else{
+          if(result.rows.length === 0){
+              res.status(403).send('username or password is invalid');
+          }else{
+              var dbString= result.row[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedpassword = hash(password,salt);
+              if(hashedpassword===dbstring){
+                  res.send('User loged successfully!');
+              }else{
+                  res.status(403).send('username or password is invalid');
+              }
+          }
+      }
+   });
+});
+*/
 
 app.post('/login', function (req, res) {
    var username = req.body.username;
@@ -169,6 +196,7 @@ app.post('/login', function (req, res) {
       }
    });
 });
+
 app.get('/hash/:input',function(req, res){
    var hashedString = hash(req.params.input,'this is any random string'); 
    res.send(hashedString);
